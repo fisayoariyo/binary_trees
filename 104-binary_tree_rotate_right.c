fe56f45c1c00d7428c440d022736cfab505d2eb7
Adefisayomi <ariyoadefisayomi@gmail.com>
2023-11-03 00:29:27 -0700

    some files had the wrong name

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -8,27 +8,27 @@
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-	binary_tree_t *pivot, *tmp;
+	binary_tree_t *driver, *tmp;
 
 	if (tree == NULL || tree->left == NULL)
 		return (NULL);
 
-	pivot = tree->left;
-	tmp = pivot->right;
-	pivot->right = tree;
+	driver = tree->left;
+	tmp = driver->right;
+	driver->right = tree;
 	tree->left = tmp;
 	if (tmp != NULL)
 		tmp->parent = tree;
 	tmp = tree->parent;
-	tree->parent = pivot;
-	pivot->parent = tmp;
+	tree->parent = driver;
+	driver->parent = tmp;
 	if (tmp != NULL)
 	{
 		if (tmp->left == tree)
-			tmp->left = pivot;
+			tmp->left = driver;
 		else
-			tmp->right = pivot;
+			tmp->right = driver;
 	}
 
-	return (pivot);
+	return (driver);
 }
